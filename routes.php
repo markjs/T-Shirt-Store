15cb15b108444a06ad5810b95b99e97fad5b6467
Mark Smith <mark@markjs.net>
2012-01-23 15:55:16 +0000

    added 404 and home page exceptions

--- routes.php
+++ routes.php
@@ -2,13 +2,22 @@
 
 $request = trim(substr($_SERVER['REQUEST_URI'],strlen($base_request)),"/");
 
+// Exception to match / to home page (index);
+if ($request == "") {
+	$request = "index";
+}
+
 // Check the page is registered with an XML file
 $page_file = "pages/".$request.".xml";
-if (file_exists($page_file)) {	
-	// Load the XML file	
-	$page_xml = simplexml_load_file($page_file);
-	// Save title from XML file into PHP variable
-	$page_title = $page_xml->title;
-	// Load relevant view
-	include "views/".$page_xml->view;
+if (!file_exists($page_file)) {	
+	$page_file = "pages/404.xml";
 }
+
+// Load the XML file	
+$page_xml = simplexml_load_file($page_file);
+
+// Save title from XML file into PHP variable
+$page_title = $page_xml->title;
+
+// Load relevant view
+include "views/".$page_xml->view;
