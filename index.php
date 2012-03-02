f7d9edb575b897d1e0c097f88b29be28376b518e
Mark Smith <mark@markjs.net>
2012-03-02 22:48:13 +0000

    neatened up register code a tad'

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 include 'functions.php';
 include 'config.php';
 include 'routes.php';
+
+session_start();
--- pages/register.php
+++ pages/register.php
@@ -4,28 +4,28 @@ $email = mysql_real_escape_string($_POST['email']);
 $password = mysql_real_escape_string($_POST['password']);
 $password_confirm = mysql_real_escape_string($_POST['password_confirm']);
 
+// Check that email address is valid
 if (is_valid_email($email)) {
   
   // Check if passwords match and md5 if they do
   if ($password == $password_confirm) {
     $password_confirm = "";
     $password = md5($password);
+    
+    // Check if a user already exists with this email address
+    $row = mysql_fetch_row(mysql_query("SELECT email FROM users WHERE email = $email"));
+    if (!isset($row['email'])) {  
+      // Create new user in database
+      $query = mysql_query("INSERT INTO users (email, password) VALUES ('$email', '$password')");
+      
+    } else {
+      // A user already exists with that email address
+    }
   } else {
     // oh dear!
-  }
-
-  // Check if a user already exists with this email address
-  $row = mysql_fetch_row(mysql_query("SELECT email FROM users WHERE email = $email"));
-  if (!isset($row['email'])) {
-    // Create new user in database
-    $query = mysql_query("INSERT INTO users (email, password) VALUES ('$email', '$password')");
-    var_dump($query);
-  } else {
-    // A user already exists with that email address
-    $error = "A user already exists with that email address";
-    include get_view_file("error");
-  }
-  
+  }  
+} else {
+  // not valid email
 }
 
 
