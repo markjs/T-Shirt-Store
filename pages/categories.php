f953da81d90eb57071f6ce6606b9201f627eadd9
Mark Smith <mark@markjs.net>
2012-03-20 15:37:36 +0000

    category listing works

---
+++ pages/categories.php
@@ -0,0 +1,29 @@
+<?php
+
+include get_view_file('header');
+
+if ($request_args[0]) {
+	$result = mysql_query("SELECT * FROM categories WHERE `slug` = '$request_args[0]' LIMIT 1");
+	
+	$category = mysql_fetch_object($result);
+	if ($category) {
+		$result = mysql_query("SELECT * FROM products WHERE `category` = '$category->id'");
+		while ($row = mysql_fetch_array($result)) {
+			$product = (object) $row;
+			include get_view_file('product_list');
+		}
+	} else {
+		// No category with that slug found
+		$error_message = "Sorry, but that category could not be found&hellip;";
+		include get_view_file('error');
+	}
+} else {
+	// No category slug has been entered
+	$result = mysql_query("SELECT * FROM categories");
+	while ($row = mysql_fetch_array($result)) {
+		$category = (object) $row;
+		include get_view_file('category_list');
+	}	
+}
+
+include get_view_file('footer');
---
+++ views/category_list.view.php
@@ -0,0 +1,1 @@
+		<li><a href="<?php echo $base_url."/categories/".$category->slug; ?>"><?php echo $category->title; ?></a></li>
